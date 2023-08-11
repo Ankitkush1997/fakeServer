ad9ef6d21a4241ef5244c3bef57becedac13ff8a
AnkitKush18 <134483795+AnkitKush18@users.noreply.github.com>
2023-08-11 17:11:12 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { createUser, signIn } from './controllers/account.js';
+import express from "express";
+import bodyParser from "body-parser";
+import { createUser, signIn } from "./controllers/account.js";
 // import { connect } from './db/connect.js';
-import { connectDB } from './db/connect.js';
-import { getSubscriptionData } from './controllers/subscription.js';
-import dotenv from 'dotenv';
+import { connectDB } from "./db/connect.js";
+import { getSubscriptionData } from "./controllers/subscription.js";
+import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    }),
+  cors({
+    exposedHeaders: ["Date"],
+  })
 );
-app.get('/', (req, res) => {
-    res.send(`Hi, i'm live`);
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.get("/", (req, res) => {
+  res.send(`Hi, i'm live`);
 });
-app.post('/sign-up', createUser);
-app.post('/sign-in', signIn);
-app.get('/subscription-plan', getSubscriptionData);
+app.post("/sign-up", createUser);
+app.post("/sign-in", signIn);
+app.get("/subscription-plan", getSubscriptionData);
 (async () => {
-    try {
-        await connectDB(process.env.MONGODB_URI);
-        app.listen(PORT, () => {
-            console.log(`server is running on ${PORT}`);
-        });
-    } catch (error) {
-        console.log('Error:', error);
-    }
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    app.listen(PORT, () => {
+      console.log(`server is running on ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error:", error);
+  }
 })();
