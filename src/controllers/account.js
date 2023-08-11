611d7201ae1c1e25af4d037403292ff0f196e4de
AnkitKush18 <134483795+AnkitKush18@users.noreply.github.com>
2023-08-11 17:32:09 +0530

    changes done

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -1,27 +1,28 @@
-import { addUser, getUser } from '../models/account.js';
+import { addUser, getUser } from "../models/account.js";
 export async function createUser(req, res) {
-    try {
-        console.log('Body:', req.body);
-        const { email, password, name } = req.body;
-        if (!email || !password || !name) throw new Error('Name, Email and Password are required');
-        const data = await addUser({ name, email, password });
-        res.send(data);
-    } catch (error) {
-        if (error instanceof Error) res.send(error.message);
-        res.status(500).send(error);
-    }
+  try {
+    console.log("Body:", req.body);
+    const { email, password, name } = req.body;
+    if (!email || !password || !name)
+      throw new Error("Name, Email and Password are required");
+    const data = await addUser({ name, email, password });
+    res.send(data);
+  } catch (error) {
+    if (error instanceof Error) res.send(error.message);
+    res.status(500).send(error);
+  }
 }
 export async function signIn(req, res) {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) throw new Error('Email and password incorrect');
-        const user = await getUser({ email, password });
-        if (!user) {
-            throw new Error('No user found!!');
-        }
-        res.send(user);
-    } catch (error) {
-        if (error instanceof Error) res.send(error.message);
-        res.status(500).send(error);
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) throw new Error("Email and password incorrect");
+    const user = await getUser({ email, password });
+    if (!user || user.length == 0) {
+      throw new Error("No user found!!");
     }
+    res.send(user);
+  } catch (error) {
+    if (error instanceof Error) res.send(error.message);
+    res.status(500).send(error);
+  }
 }
